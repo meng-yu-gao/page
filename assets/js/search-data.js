feb72c1c4c7050feb7cd9ecb3f9ec444beaf1a0a
meng-yu-gao <mgao0709@gmail.com>
2026-03-16 23:50:33 +0000

    Deploying to gh-pages from @ meng-yu-gao/page@746b78831844595b765f40a34d596b4e3da21e28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,21 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/page/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/page/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/page/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
